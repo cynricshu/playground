f0b50ae3370b2a9a80a73757bc7b8ac858438869
shulingjie <shulingjie@baidu.com>
2018-06-13 20:42:47 +0800

    grpc demo (I)

---
+++ grpc/src/main/java/RpcClient.java
@@ -0,0 +1,9 @@
+// Copyright (C) 2018 Baidu Inc. All rights reserved.
+
+/**
+ * RpcClient
+ *
+ * @author Shu Lingjie(shulingjie@baidu.com)
+ */
+public class RpcClient {
+}
---
+++ grpc/src/main/java/RpcServer.java
@@ -0,0 +1,12 @@
+// Copyright (C) 2018 Baidu Inc. All rights reserved.
+
+/**
+ * RpcService
+ *
+ * @author Shu Lingjie(shulingjie@baidu.com)
+ */
+public class RpcServer {
+
+
+
+}
